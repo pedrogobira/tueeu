bb834e8453ce1d72e089d245c738ee3b5bdf6f18
Pedro Gobira <pedrogobira@tutanota.com>
2022-11-13 18:37:15 -0300

    feat: add foreign keys to chat requests

--- database/migrations/2022_11_13_010721_create_chat_requests_table.php
+++ database/migrations/2022_11_13_010721_create_chat_requests_table.php
@@ -17,8 +17,14 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('from_user_id');
             $table->unsignedBigInteger('to_user_id');
+            $table->unsignedBigInteger('story_id');
             $table->enum('status', ['pending', 'approve', 'reject']);
             $table->timestamps();
+
+            $table->foreign('from_user_id')->references('id')->on('users');
+            $table->foreign('to_user_id')->references('id')->on('users');
+            $table->foreign('story_id')->references('id')->on('stories');
+            $table->unique(['from_user_id', 'to_user_id', 'story_id']);
         });
     }
 

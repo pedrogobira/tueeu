26006cf1909724010ebb9e93694846a12a7fb333
Pedro Gobira <pedrogobira@tutanota.com>
2022-11-15 16:03:41 -0300

    refactor: remove chat_request_id from chats table

--- database/migrations/2022_11_13_004303_create_chats_table.php
+++ database/migrations/2022_11_13_004303_create_chats_table.php
@@ -19,12 +19,10 @@ return new class extends Migration
             $table->unsignedBigInteger('to_user_id');
             $table->string('chat_message');
             $table->string('message_status');
-            $table->unsignedBigInteger('chat_request_id');
             $table->timestamps();
 
             $table->foreign('from_user_id')->references('id')->on('users');
             $table->foreign('to_user_id')->references('id')->on('users');
-            $table->foreign('chat_request_id')->references('id')->on('chat_requests');
         });
     }
 
